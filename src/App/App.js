4d2cf3da619d238a59dadee473b648d9defafc9f
Michael Schneider <michaelschneider@Michaels-MacBook-Pro.local>
2019-10-17 09:31:48 -0600

    Setup ReservationContainer as a functional componenet

--- src/App/App.js
+++ src/App/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import ReservationContainer from '../ReservationContainer/ReservationContainer';
 
 class App extends Component {
   constructor() {
@@ -24,6 +25,7 @@ class App extends Component {
 
         </div>
         <div className='resy-container'>
+          <ReservationContainer />
           
         </div>
       </div>
---
+++ src/ReservationContainer/ReservationContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './ReservationContainer.css'
+
+const ReservationContainer = () => {
+
+  return (
+    <h1>RESERVATION CONTAINER</h1>
+  )
+}
+
+export default ReservationContainer;
